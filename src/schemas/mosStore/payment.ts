d765de12c493717fa1586d07f82c0cf12950abb5
Matheus Gasparini <matheus.gasparini@dmrj.com.br>
2021-12-27 19:16:57 -0300

    TT-1800 - Ajustando validacao de numeric com ponto flutuante

--- src/schemas/mosStore/payment.ts
+++ src/schemas/mosStore/payment.ts
@@ -10,7 +10,8 @@ const createPaymentMosStoreSchema =
             "type": "number"
         },
         "value": {
-            "type": "number"
+            "type": "string",
+            "pattern": "-?^\\d*(.\\d{0,2})?$"
         },
         "installments": {
             "type": "number"
@@ -19,7 +20,7 @@ const createPaymentMosStoreSchema =
             "type": "number"
         }
     },
-    "required": ["storeId","clientId", "value", "creditCardId", "installments"]
+    "required": ["storeId", "clientId", "value", "creditCardId", "installments"]
 }
 
 const acceptPaymentMosStoreSchema =
